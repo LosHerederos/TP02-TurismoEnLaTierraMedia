f2727affa8319e60cef79bf819ca8ac4e154e256
LucilaTorales <71951782+LucilaTorales@users.noreply.github.com>
2021-10-25 17:38:14 -0300

    Actualizo inicio del sistema y quito comentarios.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -20,23 +20,7 @@ public class Main {
 		System.out.println(
 				"****************************************************************************************************\n");
 		 SistemaDeSugerencias sistema = new SistemaDeSugerencias();
-		/*
-		 * Atraccion a = new Atraccion(80,20,20,TipoDeAtracciones.AVENTURA,"Mordor");
-		 * Atraccion b = new Atraccion(28,14,14,TipoDeAtracciones.AVENTURA,"El campo");
-		 * List<Atraccion> atracciones = new LinkedList<Atraccion>();
-		 * atracciones.add(a); atracciones.add(b); PromocionPorcentual promo = new
-		 * PromocionPorcentual("Pack Aventura",atracciones,0.2);
-		 * 
-		 * //PromocionAbsoluta promoabso = (PromocionAbsoluta) promo; //promo =
-		 * (PromocionPorcentual) promo; System.out.println(promo.getCosto());
-		 * System.out.println(promo.getTiempo());
-		 * System.out.println(promo.getTipoDeAtraccion());
-		 * System.out.println(promo.getNombre());
-		 * System.out.println(promo.getAtraccion());
-		 * System.out.println(promo.reduccionCostoTotal());
-		 * //System.out.println(promoabso.getDescuento());
-		 * //System.out.println(promoabso.cupoCompleto());
-		 */ // System.out.println(String.format("%-22s\n", promo.getNombre()));
+		 sistema.iniciarMenu();
 	}
 
 }
